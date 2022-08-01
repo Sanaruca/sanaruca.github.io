175c27aa1182ee123b64624b3b844fd2ca5301cd
sanaruca <sanaruca@gmail.com>
2022-08-01 07:31:51 -0400

    image slider working

--- routes.ts
+++ routes.ts
@@ -15,8 +15,9 @@ export const appRoutes: Route[] = [
             locals: {
                 title: 'sanaruca',
                 project: {
-                    name: "shoppingapp",
-                    description: "Shoppingapp is a face-to-face development android app written with React Native. Its function is to help keep track of the purchases that are made regularly in the supermarket in order to save on purchase costs by writing down each product that is being deposited in the shopping cart. In other words, while the user is in the supermarket, he can write down the prices of the products he is going to take."
+                    name: 'shoppingapp',
+                    description: "Shoppingapp is a face-to-face development android app written with React Native. Its function is to help keep track of the purchases that are made regularly in the supermarket in order to save on purchase costs by writing down each product that is being deposited in the shopping cart. In other words, while the user is in the supermarket, he can write down the prices of the products he is going to take.",
+                    usedSkills: ['react-native', 'explo', 'typescript', 'mobile']
                 } as Project
             }
         }
